f68284aea2037993f2359865927aabbdb38f7076
Ilhom Karimov <ilhom.uz@gmail.com>
2016-09-16 20:47:23 -0400

    Initial commit, static bootstrap templates configured and ready

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+
+//Express instance
+var app = express();
+
+//server port
+var port = 5000;
+
+//Adding static files
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/src/views'));
+
+app.get('/', function (req, res) {
+    res.send("Hello world");
+    
+});
+
+app.listen(port, function (err) {
+    console.log('running server on port:', port);
+    
+});
